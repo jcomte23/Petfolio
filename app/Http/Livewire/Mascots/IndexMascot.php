fd385eed559b8d321b031218edaa7987f98a4561
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-07-27 07:39:00 -0500

    creaciondel primer componente livewire

---
+++ app/Http/Livewire/Mascots/IndexMascot.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Mascots;
+
+use Livewire\Component;
+
+class IndexMascot extends Component
+{
+    public function render()
+    {
+        return view('livewire.mascots.index-mascot');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Livewire\Mascots\IndexMascot;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,7 +23,8 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified'
 ])->group(function () {
-    Route::get('/dashboard', function () {
+    Route::get('dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+    Route::get('mascots',IndexMascot::class)->name('mascots.index');
 });
